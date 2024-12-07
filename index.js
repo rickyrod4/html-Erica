84e6e1bad56141e955dd801c8221e8a4646b6503
rickyrod4 <ricardo.rodriguez4@yahoo.com>
2024-12-07 14:36:34 -0600

    added jquery to index.js to change the h1 on click

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function playSound(){
     var crash1 = new Audio('/sound/crash.mp3');
         crash1.play();
 }
+
+$(document).click(function(){
+    $('h1').animate({
+        opacity: 0
+    }, 500, function(){
+        $(this).text("I Love You Mami!!!").animate({
+            opacity: 1
+        }, 500);
+    });
+});
